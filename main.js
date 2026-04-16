84bad8505c88c53704f1ac156dc2329d4c887f69
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-16 15:01:58 +0530

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
 
 const loader = new PDFLoader("./story.pdf");
 
 const docs = await loader.load();
 
-console.log(docs); 
+// console.log(docs);
+
+const embeddings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 0,
+});
+
+const chunks = await splitter.splitDocuments(docs);
+
+console.log(chunks, chunks.length);
