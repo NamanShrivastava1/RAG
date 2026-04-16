0700f312a7c466f7ba3ef141a4b285bc0c732ebd
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-04-16 15:29:53 +0530

    Refactor main.js for improved data handling and processing flow

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { MistralAIEmbeddings } from "@langchain/mistralai";
 
+// Step 1
 const loader = new PDFLoader("./story.pdf");
+const data = await loader.load();
+// console.log(data)
 
-const docs = await loader.load();
-
-// console.log(docs);
-
+//Step 3
 const embeddings = new MistralAIEmbeddings({
   apiKey: process.env.MISTRAL_API_KEY,
+  model: "mistral-embed",
 });
 
+// Step 2
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 500,
-  chunkOverlap: 0,
+  chunkOverlap: 50,
 });
 
-const chunks = await splitter.splitDocuments(docs);
+const chunks = await splitter.splitDocuments(data);
+// console.log(chunks)
 
-console.log(chunks, chunks.length);
+const texts = chunks.map((doc) => doc.pageContent);
+
+const vectors = await Promise.all(
+  texts.map(async (chunk) => {
+    const embedding = await embeddings.embedQuery(chunk);
+    return {
+      text: chunk,
+      embedding,
+    };
+  }),
+);
+
+console.log(vectors); //1024 Dimensions
